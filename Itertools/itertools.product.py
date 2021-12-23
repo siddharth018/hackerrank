736fdcc6a9cb99cfff2fa130cd2295ff6c6170ff
shivamyada <jay08881610484@gmail.com>
2021-12-23 23:41:10 +0530

    update code

--- Itertools/itertools.product.py
+++ Itertools/itertools.product.py
@@ -5,7 +5,7 @@
 # This tool computes the cartesian product of input iterables. 
 # It is equivalent to nested for-loops. 
 # For example, product(A, B) returns the same as ((x,y) for x in A for y in B).
-
+# Shivsm
 # Task
 # You are given a two lists A and B. Your task is to compute their cartesian product X.
 
